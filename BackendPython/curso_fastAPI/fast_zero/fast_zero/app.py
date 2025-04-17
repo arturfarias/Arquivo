4c5189017d41488f07269d42ecdcb365be3f2929
Artur S Farias <artur06farias@gmail.com>
2025-04-17 12:43:55 -0300

    post

--- BackendPython/curso_fastAPI/fast_zero/fast_zero/app.py
+++ BackendPython/curso_fastAPI/fast_zero/fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserPublic, UserSchema, UserDB
 
 app = FastAPI()
 
 
-@app.get('/')
+database = []
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    
+    user_with_id = UserDB(
+        id = len(database) + 1,
+        **user.model_dump()
+    )
+
+    return user_with_id
