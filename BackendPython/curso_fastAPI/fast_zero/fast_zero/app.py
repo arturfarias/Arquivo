b42db81e8d9c4438c8cbfee48b9267bcbcc75030
Artur S Farias <artur06farias@gmail.com>
2025-05-28 15:51:31 -0300

    crud

--- BackendPython/curso_fastAPI/fast_zero/fast_zero/app.py
+++ BackendPython/curso_fastAPI/fast_zero/fast_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException 
+from fastapi import FastAPI, HTTPException
 
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
@@ -28,15 +28,29 @@ def create_user(user: UserSchema):
 def read_users():
     return {'users': database}
 
-@app.put('/users{user_id}/', status_code=HTTPStatus.OK, response_model=UserPublic)
+
+@app.put(
+    '/users/{user_id}/', status_code=HTTPStatus.OK, response_model=UserPublic
+)
 def update_user(user_id: int, user: UserSchema):
     user_with_id = UserDB(id=user_id, **user.model_dump())
     if user_id < 1 or user_id > len(database):
         raise HTTPException(
-            status_code= HTTPStatus.NOT_FOUND,
-            detail = 'Não foi encontrado o id indicado'
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Não foi encontrado o id indicado',
         )
-
-    database[user_id -1] = user_with_id
+    database[user_id - 1] = user_with_id
 
     return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}/', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_users(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Não foi encontrado o id indicado',
+        )
+    return database.pop(user_id - 1)
--- BackendPython/curso_fastAPI/fast_zero/tests/test_app.py
+++ BackendPython/curso_fastAPI/fast_zero/tests/test_app.py
@@ -40,3 +40,51 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'updatedUserName',
+            'email': 'updated@teste.com',
+            'password': 'updated',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'updatedUserName',
+        'email': 'updated@teste.com',
+    }
+
+
+def test_update_user_NOT_FOUND(client):
+    response = client.put(
+        '/users/9999',
+        json={
+            'username': 'updatedUserName',
+            'email': 'updated@teste.com',
+            'password': 'updated',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'updatedUserName',
+        'email': 'updated@teste.com',
+    }
+
+
+def test_delete_user_NOT_FOUND(client):
+    response = client.delete('/users/9999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
