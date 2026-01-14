73cc438ddc5ea6657a31664fc7be2cc0d6ae8226
Artur S Farias <artur06farias@gmail.com>
2026-01-14 13:32:55 -0300

    response to route

--- BackendNode/Express-Typescript/src/index.ts
+++ BackendNode/Express-Typescript/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { Request, Response } from "express";
 
-
 const app = express()
 const port = 3000
 
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ message: 'Hello World!'});
-});
+app.get("/", (req: Request, res: Response): Response => {
+    return res.json({ message: "Hello World!" });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
