787084a54d30f849a70a278588389e5af487bc89
Artur S Farias <artur06farias@gmail.com>
2026-02-12 13:19:20 -0300

    bcrypt

--- BackendNode/Express-Typescript-JWT/src/routes/user.ts
+++ BackendNode/Express-Typescript-JWT/src/routes/user.ts
@@ -3,21 +3,52 @@ import { Router } from "express";
 import { Request, Response } from "express";
 import createInMemoryDatabase from "../fakeDB";
 
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import User from "../interfaces/user"
 const userDb = createInMemoryDatabase<User>();
 
 
 const userRouter  = Router();
 
-userRouter.post("/user", (req: Request, res: Response): Response => {
-    const {name,email, password} = req.body;
+userRouter.post("/user", async (req: Request, res: Response): Promise<Response> => {
+    const {id, name, email, password} = req.body;
 
     const userExists = userDb.findByEmail(email);
+
+    if(userExists) {
+      return res.status(400).json({ error: "Email ja cadastrado" });
+    }
+
+    const hashPassword = await bcrypt.hash(password,10)
     
-    return res.json({ message: userExists });
+    const newUser = await userDb.create({id, name, email, password:hashPassword});
+    
+    return res.status(201).json(newUser);
   }
 );
 
+userRouter.post("/login", async (req: Request, res: Response): Promise<Response> => {
+    const {id, name, email, password} = req.body;
+
+    const user = userDb.findByEmail(email);
+
+    if(!user) {
+      return res.status(400).json({ error: "Email ou senha invalidos" });
+    }
+
+    const verifyPass = await bcrypt.compare(password, user.password);
+
+    if(!verifyPass) {
+      return res.status(400).json({ error: "Email ou senha invalidos" });
+    }
+
+    const token = jwt.sign({},"dddfdsfds")
+    
+    return res.status(200).json({});
+  }
+);
 
 
 export default userRouter;
